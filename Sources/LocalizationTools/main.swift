216cc93c9327b32b4e6f44fdda1feb7c3d2d84ee
Jeff Boek <j@jboek.com>
2021-08-12 11:35:50 -0700

    Adds ArgumentParser dependency

--- Sources/LocalizationTools/main.swift
+++ Sources/LocalizationTools/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct LocalizationTools: ParsableCommand {
+    mutating func run() throws {
+        print("Hello world")
+    }
+}
+
+LocalizationTools.main()
